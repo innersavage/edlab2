47177de77395f4f4fbe2b5f8e862b474e05c8179
luc <luc@slon.lotnik.net>
2020-12-06 04:49:14 +0100

    Zadanie 4 + zapisane ploty w formie png

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import matplotlib.lines
 from sklearn.decomposition import PCA
 import seaborn
 import numpy
+from sklearn.metrics import silhouette_samples, silhouette_score
 
 # Zadanie 1 (1 pkt.)
 print('W pocie czoła ładuję dane...')
@@ -88,7 +89,67 @@ subplot_dbscan.grid(True)
 print(sample_dataset)
 
 # Zadanie 4
+# A teraz nauczę się czym jest silhouette coeficiency, jest 4:36 i  padam na twarz :P
+sample_dataset = source_dataset.copy()
+vis_dataset = pandas.DataFrame(pca_2d.fit_transform(sample_dataset))
+for n_clusters in range(2, 8):
+    # Create a subplot with 1 row and 2 columns
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig.set_size_inches(18, 7)
+
+    ax1.set_xlim([-0.1, 1])
+    ax1.set_ylim([0, len(vis_dataset) + (n_clusters + 1) * 10])
+    clusterer = cluster.KMeans(n_clusters=n_clusters, random_state=10)
+    cluster_labels = clusterer.fit_predict(vis_dataset)
+
+    silhouette_avg = silhouette_score(vis_dataset, cluster_labels)
+    print("For n_clusters =", n_clusters,
+          "The average silhouette_score is :", silhouette_avg)
+
+    # Compute the silhouette scores for each sample
+    sample_silhouette_values = silhouette_samples(vis_dataset, cluster_labels)
+
+    y_lower = 10
+    for i in range(n_clusters):
+        ith_cluster_silhouette_values = sample_silhouette_values[cluster_labels == i]
+
+        ith_cluster_silhouette_values.sort()
+
+        size_cluster_i = ith_cluster_silhouette_values.shape[0]
+        y_upper = y_lower + size_cluster_i
+
+        color = matplotlib.cm.nipy_spectral(float(i) / n_clusters)
+        ax1.fill_betweenx(numpy.arange(y_lower, y_upper),
+                          0, ith_cluster_silhouette_values,
+                          facecolor=color, edgecolor=color, alpha=0.7)
+        ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+        y_lower = y_upper + 10
+
+    ax1.axvline(x=silhouette_avg, color="red", linestyle="--")
+
+    ax1.set_yticks([])
+    ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
+
+    # 2nd Plot showing the actual clusters formed
+    colors = matplotlib.cm.nipy_spectral(cluster_labels.astype(float) / n_clusters)
+    ax2.scatter(vis_dataset[0], vis_dataset[1], marker='.', s=30, lw=0, alpha=0.7,
+                c=colors, edgecolor='k')
+
+    centers = clusterer.cluster_centers_
+    ax2.scatter(centers[:, 0], centers[:, 1], marker='o',
+                c="white", alpha=1, s=200, edgecolor='k')
+
+    for i, c in enumerate(centers):
+        ax2.scatter(c[0], c[1], marker='$%d$' % i, alpha=1,
+                    s=50, edgecolor='k')
+
+    ax2.set_title("The visualization of the clustered data.")
+    ax2.set_xlabel("Feature space for the 1st feature")
+    ax2.set_ylabel("Feature space for the 2nd feature")
+
+    plt.suptitle(("Silhouette analysis for KMeans clustering on sample data "
+                  "with n_clusters = %d" % n_clusters),
+                 fontsize=14, fontweight='bold')
 
 # Koniec
 plt.show()
-print('Co tu się odjaniepawla?')
